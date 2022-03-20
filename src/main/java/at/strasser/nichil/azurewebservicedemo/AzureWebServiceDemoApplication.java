cee3d6563516ce4b173586249acecd51b53ac6a4
Nichil Strasser <nichil.strasser@me.com>
2022-03-20 18:16:10 +0100

    chore: build application skeleton

---
+++ src/main/java/at/strasser/nichil/azurewebservicedemo/AzureWebServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package at.strasser.nichil.azurewebservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureWebServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureWebServiceDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/strasser/nichil/azurewebservicedemo/AzureWebServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package at.strasser.nichil.azurewebservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureWebServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
